37a85a1bc583e71418feb2f2ff5a1b556c102d4b
Reskyaprilia <resky8196@students.amikom.ac.id>
2016-11-19 22:55:59 +0700

    File1File2

---
+++ File2.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Modifying Elements</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $Friends = array("Friends",
+        "Amel", "Kiky", "Desty", "Ika");
+        $fr[3] = "Amel";
+        
+        echo $Friends[3];
+      ?>
+    </p>
+  </body>
+</html>
